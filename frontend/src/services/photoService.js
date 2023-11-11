55d19175a29c539a7b8c09dfb9f07daac18e5c69
Eros Netto <erosnetto1002@gmail.com>
2023-11-10 23:55:57 -0300

    Comentários feito

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -90,6 +90,21 @@ const like = async (id, token) => {
   }
 };
 
+// Add comment to a photo
+const comment = async (data, id, token) => {
+  const config = requestConfig("PUT", data, token);
+
+  try {
+    const res = await fetch(api + "/photos/comment/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
@@ -97,6 +112,7 @@ const photoService = {
   updatePhoto,
   getPhoto,
   like,
+  comment,
 };
 
 export default photoService;
--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -103,6 +103,27 @@ export const like = createAsyncThunk("photo/like", async (id, thunkAPI) => {
   return data;
 });
 
+// Add comment to a photo
+export const comment = createAsyncThunk(
+  "photo/comment",
+  async (commentData, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.comment(
+      { comment: commentData.comment },
+      commentData.id,
+      token
+    );
+
+    // Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -208,6 +229,19 @@ export const photoSlice = createSlice({
         state.message = action.payload.message;
       })
       .addCase(like.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(comment.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        state.photo.comments.push(action.payload.comment);
+
+        state.message = action.payload.message;
+      })
+      .addCase(comment.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
       });
