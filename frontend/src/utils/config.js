edc6361c672a223318254b2746e67ebbee90d0f1
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-08-27 16:46:22 -0300

    Att

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,7 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+// export const api = "http://localhost:5000/api";
+// export const uploads = "http://localhost:5000/uploads";
+export const api = "https://a-rede-social.onrender.com/api";
+export const uploads = "https://a-rede-social.onrender.com/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
