bffd6bb9938376388990b41e2294565e089b9e34
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-11-01 10:34:18 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://aredesocial.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://aredesocial-eros.vercel.app",
+      "https://aredesocial.vercel.app",
+    ],
   })
 );
 
